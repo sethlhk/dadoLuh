0ee4970122c66e3b7ae223ec89a5f969e4660260
sethlhk <lauramodesto465@gmail.com>
2023-04-27 17:17:27 -0300

    dado

--- app/src/main/java/com/example/dadoluh/MainActivity.kt
+++ app/src/main/java/com/example/dadoluh/MainActivity.kt
@@ -3,44 +3,79 @@ package com.example.dadoluh
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.dadoluh.ui.theme.DadoLuhTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DadoLuhTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+           DadoLuhTheme{
+               RolaDado()
                 }
+            
             }
         }
     }
-}
 
+
+
+
+
+
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun RolaDado(){
+ var resultado= 1
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "1d6",
+        fontSize = 35.sp,
+        color = Color.Black,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(50.dp),
+        fontWeight =  FontWeight.Bold
     )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DadoLuhTheme {
-        Greeting("Android")
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource
+            (
+            id =R.drawable.dice_1
+            )
+            ,contentDescription =null
+            )
+            Button(onClick = { resultado= (1..6).random()
+            }
+            )
+            {
+                Text(stringResource(id = R.string.roll )
+                )
+            }
     }
+    
 }
